70afc6c214988faf106a87f6284c19a05d9bd2e3
Mogreine <north7311@gmail.com>
2020-03-13 20:02:08 +0400

    Implemented coords retrieving

--- ArduinoCode/src/CameraWebServer/app_httpd.cpp
+++ ArduinoCode/src/CameraWebServer/app_httpd.cpp
@@ -1,16 +1,4 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#define _USE_MATH_DEFINES
 #include "esp_http_server.h"
 #include "esp_timer.h"
 #include "esp_camera.h"
@@ -18,8 +6,7 @@
 #include "camera_index.h"
 #include "Arduino.h"
 #include <string>
-#include <sstream>
-#include <vector>
+#include <cmath>
 
 #include "fb_gfx.h"
 #include "fd_forward.h"
@@ -67,15 +54,201 @@ static int8_t recognition_enabled = 0;
 static int8_t is_enrolling = 0;
 static face_id_list id_list = {0};
 
-void split(string& init_str, vector<string>& buff) {
-  stringstream test;
-  test << init_str;
-  string segment;
+#define IN1 4 
+#define IN2 2 
+#define IN3 14
+#define IN4 15
 
-  while(getline(test, segment, '&'))
-  {
-    buff.push_back(segment);
+class Car {
+private:
+  int left_forward_pin;
+  int left_backward_pin;
+  int right_forward_pin;
+  int right_backward_pin;
+
+  int left_forward_channel;
+  int left_backward_channel;
+  int right_forward_channel;
+  int right_backward_channel;
+
+  void set_ledc(int channel_init, int resolution = 8, int freq = 1000) {
+    // setting up left motor
+    ledcSetup(channel_init, freq, resolution);
+    ledcAttachPin(left_forward_pin, channel_init);
+    left_forward_channel = channel_init;
+    channel_init++;
+
+    ledcSetup(channel_init, freq, resolution);
+    ledcAttachPin(left_backward_pin, channel_init);
+    left_backward_channel = channel_init;
+    channel_init++;
+
+    // setting up right motor
+    ledcSetup(channel_init, freq, resolution);
+    ledcAttachPin(right_forward_pin, channel_init);
+    right_forward_channel = channel_init;
+    channel_init++;
+
+    ledcSetup(channel_init, freq, resolution);
+    ledcAttachPin(right_backward_pin, channel_init);
+    right_backward_channel = channel_init;
   }
+
+  void set_left_speed(int speed) {
+    ledcWrite(left_forward_channel, backwards == 0 ? speed : 0);
+    ledcWrite(left_backward_channel, backwards == 1 ? speed : 0);
+    left_speed = speed;
+  }
+
+  void set_right_speed(int speed) {
+    ledcWrite(right_forward_channel, backwards == 0 ? speed : 0);
+    ledcWrite(right_backward_channel, backwards == 1 ? speed : 0);
+    right_speed = speed;
+  }
+public:
+  int left_speed;
+  int right_speed;
+  bool backwards;
+
+  Car(int _left_forward_pin, int _left_backward_pin, int _right_forward_pin, int _right_backward_pin) {
+    left_forward_pin = _left_forward_pin;
+    left_backward_pin = _left_backward_pin;
+    right_forward_pin = _right_forward_pin;
+    right_backward_pin = _right_backward_pin;
+
+    set_ledc(4);
+    backwards = 0;
+    set_speed(0, 0, 0);
+  }
+
+  void set_direction(bool back) {
+    backwards = back;
+    set_speed(left_speed, right_speed, back);
+  }
+
+  void set_speed(int left, int right, bool back) {
+    backwards = back;
+    set_left_speed(left);
+    set_right_speed(right);
+  }
+
+  void parse_polar_coords(double r, double angle) {
+    r = min(1.0, r);
+    r = max(0.0, r);
+    if (r < 1e-5) {
+      set_speed(0, 0, 0);
+      return;
+    }
+    int min_speed = 50,
+        max_diff = 205;
+    if (angle < M_PI_2) {
+      set_speed(min_speed + r * max_diff, min_speed + r * sin(angle) * max_diff, 0);
+    }
+    else if (angle < M_PI) {
+      set_speed(min_speed + r * sin(angle) * max_diff, min_speed + r * max_diff, 0);
+    }
+    else if (angle < 3 * M_PI / 2) {
+      set_speed(min_speed + r * abs(sin(angle)) * max_diff, min_speed + r * max_diff, 1);
+    }
+    else {
+      set_speed(min_speed + r * max_diff, min_speed + r * abs(sin(angle)) * max_diff, 1);
+    }
+  }
+
+  void parse_coords(double x, double y) {
+    // validation
+    x = min(1.0, x);
+    x = max(-1.0, x);
+    y = min(1.0, y);
+    y = max(-1.0, y);
+
+    double r = sqrt(x * x + y * y);
+    if (r < 1e-5) {
+      set_speed(0, 0, 0);
+      return;
+    }
+    int min_speed = 60,
+        max_diff = 160;
+    if (x > 0 && y > 0) {
+      set_speed(min_speed + r * max_diff, min_speed + r * y * max_diff, 0);
+    }
+    else if (x < 0 && y > 0) {
+      set_speed(min_speed + r * y * max_diff, min_speed + r * max_diff, 0);
+    }
+    else if (x < 0 && y < 0) {
+      set_speed(min_speed + r * (-y) * max_diff, min_speed + r * max_diff, 1);
+    }
+    else {
+      set_speed(min_speed + r * max_diff, min_speed + r * (-y) * max_diff, 1);
+    }
+  }
+};
+
+void split(const char* str, int sz, const char delim, char** buff, int tokens = 2) {
+  //20 - max token size
+  char curr_str[20];
+  // i - str iterator, j - curr_str iterator, k - buff iterator
+  int i = 0, j = 0, k = 0;
+  for(; i < sz && k < tokens; i++) {
+    if (str[i] == delim) {
+      curr_str[j] = '\0';
+      strcpy(buff[k], curr_str);
+      k++;
+      j = 0;
+      memset(curr_str, '\0', 20);
+      Serial.printf("%s\n", buff[k - 1]);
+    }
+    else {
+      curr_str[j] = str[i];
+      j++;
+    }
+  }
+  if (i == sz && k < tokens) {
+    curr_str[j] = '\0';
+    strcpy(buff[k], curr_str);
+    Serial.printf("%s\n", buff[k]);
+  }
+}
+
+void parse_request(const char* req, int sz, float& x, float& y) {
+  Serial.println("Started splitting");
+  int max_tokens = 2;
+  char** prms = (char**)malloc(max_tokens * sizeof(char*));
+  int val_size = 30;
+  for(int i = 0; i < max_tokens; i++) {
+    prms[i] = (char*)malloc(val_size * sizeof(char));
+  }
+  split(req, sz, '&', prms, max_tokens);
+  Serial.println("Finished splitting");
+  // Serial.printf("Part1: %s, part2: %s\n", prms[0], prms[1]);
+  
+  Serial.println("Retrieving param1");
+  char** tmp_param_arr = (char**)malloc(max_tokens * sizeof(char*));
+  for(int i = 0; i < max_tokens; i++) {
+    tmp_param_arr[i] = (char*)malloc(val_size * sizeof(char));
+  }
+  split(prms[0], strlen(prms[0]), '=', tmp_param_arr, max_tokens);
+  Serial.println("Retrieving complete");
+  x = atof(tmp_param_arr[1]);
+  Serial.printf("x: %f\n", x);
+  
+  for(int i = 0; i < max_tokens; i++) {
+    memset(tmp_param_arr[i], 0, val_size);
+  }
+  
+  Serial.println("Retrieving param2");
+  split(prms[1], strlen(prms[1]), '=', tmp_param_arr, max_tokens);
+  Serial.println("Retrieving complete");
+  y = atof(tmp_param_arr[1]);
+  Serial.printf("y: %f\n", y);
+
+  Serial.println("Freeing memory");
+  for(int i = 0; i < max_tokens; i++) {
+    free(prms[i]);
+    free(tmp_param_arr[i]);
+  }
+  free(prms);
+  free(tmp_param_arr);
 }
 
 static ra_filter_t * ra_filter_init(ra_filter_t * filter, size_t sample_size){
@@ -241,14 +414,10 @@ static esp_err_t capture_handler(httpd_req_t *req){
     char res_data [res_data_size]; 
     int read_bytes = httpd_req_recv(req, res_data, res_data_size);
     Serial.printf("Read bytes: %d, req data: %s\n", read_bytes, res_data);
-    string res_data_str(res_data);
-    Serial.printf("Req data: %s\n", res_data_str.c_str());
-//    vector<string> segments;
-//    split(res_data_str, segments);
-//    // Serial.printf("Req data: %s\n", res_data_str.c_str());
-//    for (auto segment : segments) {
-//      Serial.printf("Req data: %s\n", segment.c_str());
-//    }
+
+    float x = 0, y = 0;
+    parse_request(res_data, read_bytes, x, y);
+    Serial.printf("value1: %f, value2: %f\n", x, y);  
     
     fb = esp_camera_fb_get();
     if (!fb) {
@@ -331,6 +500,7 @@ static esp_err_t capture_handler(httpd_req_t *req){
     Serial.printf("FACE: %uB %ums %s%d\n", (uint32_t)(jchunk.len), (uint32_t)((fr_end - fr_start)/1000), detected?"DETECTED ":"", face_id);
     return res;
 }
+
 
 static esp_err_t stream_handler(httpd_req_t *req){
     camera_fb_t * fb = NULL;
