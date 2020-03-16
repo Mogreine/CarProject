87fd7336f7a87304d134a331007cb72baac75bc8
Mogreine <north7311@gmail.com>
2020-03-16 15:14:28 +0400

    Fixed amazing code

---
+++ ArduinoCode/src/CameraWebServer/SPID.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <NewPing.h>
+
+class SPID {
+public:
+    double prop_num = 80;
+    double integ_num = 1.5;
+    double diff_num = 0.5;
+
+    double integ_max = 0.2;
+    double integ_min = -0.2;
+    double integ_sum = 0;
+    double prev_val = -1;
+
+    double req_dist = 0.5; // m
+
+    NewPing *sonar;
+
+    SPID();
+    SPID(NewPing *sonar);
+    double calculate(double new_val);
+    double calculate();
+};
